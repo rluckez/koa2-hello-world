bfed3fb1a4b71133c8bafdcfeaa8805a3ae477bb
Rafael <rluckez@outlook.com>
2019-01-13 19:42:42 -0200

    Remove unused files

--- mongoimport.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mongoimport --db test --collection users --drop --file data/users.json --jsonArray
-mongoimport --db test --collection companies --drop --file data/companies.json --jsonArray
